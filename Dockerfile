cef404c17cfecd9d1b9c67dd8318a930aadbbc10
Anton Kharchenko <anton.kharchenko@pxpfinancial.com>
2024-04-10 17:23:36 +0300

    loki + docker

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+﻿FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build-env
+WORKDIR /App
+
+# Copy everything
+COPY . ./
+# Restore as distinct layers
+RUN dotnet restore TFA.Api
+# Build and publish a release
+RUN dotnet publish TFA.Api -c Release -o out
+
+# Build runtime image
+FROM mcr.microsoft.com/dotnet/aspnet:8.0
+WORKDIR /App
+COPY --from=build-env /App/out .
+
+CMD dotnet TFA.API.dll
--- TFA.Api/Extensions/LoggingServiceCollectionExtensions.cs
+++ TFA.Api/Extensions/LoggingServiceCollectionExtensions.cs
@@ -1,22 +1,48 @@
-﻿using Serilog;
+﻿using System.Diagnostics;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 using Serilog.Filters;
+using Serilog.Sinks.Grafana.Loki;
 
 namespace TFA.Api.Extensions;
 
 internal static class LoggingServiceCollectionExtensions
 {
-    public static IServiceCollection AddApiLogging(this IServiceCollection services,
+    private static readonly string[] PropertiesAsLabels =
+    [
+        "level", "Environment", "Application", "SourceContext"
+    ];
+
+    public static void AddApiLogging(this IServiceCollection services,
         IConfiguration configuration,
         IWebHostEnvironment environment)
     {
-        return services.AddLogging(b => b.AddSerilog(new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .Enrich.WithProperty("Application", "TFA.API")
-            .Enrich.WithProperty("Environment", environment.EnvironmentName)
-            .WriteTo.Logger(lc => lc
-                .Filter.ByExcluding(Matching.FromSource("Microsoft"))
-                .WriteTo.OpenSearch(configuration.GetConnectionString("Logs"), "forum-logs-{0:yyyy.MM.dd}"))
-            .WriteTo.Logger(lc => lc.WriteTo.Console())
-            .CreateLogger()));
+        services.AddLogging(loggingBuilder => loggingBuilder
+            .Configure(options => options.ActivityTrackingOptions =
+                ActivityTrackingOptions.TraceId | ActivityTrackingOptions.SpanId)
+            .AddSerilog(new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.WithProperty("Application", "TFA.API")
+                .Enrich.WithProperty("Environment", environment.EnvironmentName)
+                .Enrich.With<TracingContextEnricher>()
+                .WriteTo.Logger(lc => lc
+                    .Filter.ByExcluding(Matching.FromSource("Microsoft"))
+                    .WriteTo.GrafanaLoki(configuration.GetConnectionString("Logs")!,
+                        propertiesAsLabels: PropertiesAsLabels, leavePropertiesIntact: true))
+                .WriteTo.Logger(lc => lc.WriteTo.Console())
+                .CreateLogger()));
+    }
+
+    private class  TracingContextEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var activity = Activity.Current;
+            if(activity is null) return;
+            
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("TraceId",  activity.TraceId));
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("SpanId", activity.SpanId));
+        }
     }
 }
