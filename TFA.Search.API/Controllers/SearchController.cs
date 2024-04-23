ccb5433bef93a7e5167d25be273389b5091c0eb6
Anton Kharchenko <anton.kharchenko@pxpfinancial.com>
2024-04-23 10:11:47 +0300

    Add search proto file for gRPC communication

--- TFA.Search.API/Controllers/SearchController.cs
+++ TFA.Search.API/Controllers/SearchController.cs
@@ -6,24 +6,27 @@ using TFA.Search.Domain.Queries;
 
 namespace TFA.Search.API.Controllers;
 
-public class SearchController(IMediator mediator) : ControllerBase
+public class SearchController(ISender mediator) : ControllerBase
 {
     [HttpPost("index")]
     public async Task<IActionResult> Index([FromBody] SearchEntity searchEntity, CancellationToken cancellationToken)
     {
-        var command = new IndexCommand(searchEntity.EntityId, searchEntity.SearchEntityType, searchEntity.Title,
+        var command = new IndexCommand(
+            searchEntity.EntityId,
+            searchEntity.SearchEntityType,
+            searchEntity.Title,
             searchEntity.Text!);
 
         await mediator.Send(command, cancellationToken);
 
         return Ok();
     }
-    
+
     [HttpGet("search")]
     public async Task<IActionResult> Search(string query, CancellationToken cancellationToken)
     {
-        var result = await mediator.Send(new SearchQuery(query), cancellationToken);
-       
-        return Ok(result);
+        var (resources, totalCount) = await mediator.Send(new SearchQuery(query), cancellationToken);
+
+        return Ok(new { resources, totalCount });
     }
 }
